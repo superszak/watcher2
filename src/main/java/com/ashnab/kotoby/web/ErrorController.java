920dd129d902d096ed7af9da43780576dbf78730
admin1 <superszak@wp.pl>
2016-05-31 17:25:04 +0200

    again

--- src/main/java/com/ashnab/kotoby/web/ErrorController.java
+++ src/main/java/com/ashnab/kotoby/web/ErrorController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ErrorController {
-
+/*
     @RequestMapping("/error")
     public String error() {
         return "error";
     }
-
+*/
 }
