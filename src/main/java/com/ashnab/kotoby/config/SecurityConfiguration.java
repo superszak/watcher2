9eccc91bba20dfe59de76e66066f0b64f8e43d24
admin1 <superszak@wp.pl>
2016-05-31 18:18:32 +0200

    again

--- src/main/java/com/ashnab/kotoby/config/SecurityConfiguration.java
+++ src/main/java/com/ashnab/kotoby/config/SecurityConfiguration.java
@@ -32,6 +32,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf()
+                .disable()
                 .formLogin()
                 .loginPage("/login")
                 .loginProcessingUrl("/login/authenticate")
